baf2c9537ba57f20a4bc8362fe23e643fee9a91e
Claire Oxner <choxner@wharton.upenn.edu>
2022-12-20 15:42:01 -0500

    Adjusted path for get_azm_time_series

--- R/activity_zone_minutes.R
+++ R/activity_zone_minutes.R
@@ -6,7 +6,7 @@ url_active_zone_minutes <- paste0(url_api, "activities/active-zone-minutes/")
 #' @title Get Activity Zone Minutes Time Series
 #'
 #' @description
-#'   \code{get_activity_time_series()} returns time series data in the specified range for a given resource.
+#'   \code{get_azm_time_series()} returns time series data in the specified range for a given resource.
 #'
 #' @inheritParams inheritparams_token
 #' @param base_date The range start date. A Date class object or a string in the format yyyy-MM-dd or today.
@@ -24,9 +24,9 @@ url_active_zone_minutes <- paste0(url_api, "activities/active-zone-minutes/")
 get_azm_time_series <- function(token, date="", period="", base_date="", end_date="", simplify=TRUE)
 {
   url <- if(date != "" && period != ""){
-    paste0(url_active_zone_minutes, sprintf("%s/date/%s/%s.json", format_date(date), period))
+    paste0(url_active_zone_minutes, sprintf("date/%s/%s.json", format_date(date), period))
   } else if(base_date != "" & end_date != ""){
-    paste0(url_active_zone_minutes, sprintf("%s/date/%s/%s.json", format_date(base_date), format_date(end_date)))
+    paste0(url_active_zone_minutes, sprintf("date/%s/%s.json", format_date(base_date), format_date(end_date)))
   } else{
     stop("Error: Need to enter combination of date/period or base_date/end_date")
   }
